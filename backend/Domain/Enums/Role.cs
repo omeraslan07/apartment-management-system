5f98d28235af32f57e5aff5f2e8acfebdf059017
baranacikgoz <baran-acikgoz@outlook.com>
2022-04-01 01:30:49 +0300

    initialized backend template

---
+++ backend/Domain/Enums/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Enums
+{
+    public enum Role
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
---
+++ backend/Infrastructure.Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Enums;
+
+namespace Infrastructure.Identity.Seeds
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Role.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Role.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Role.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Role.Basic.ToString()));
+        }
+    }
+}
